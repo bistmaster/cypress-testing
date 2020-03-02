c8447bb40fc091d4f71d94e3fc7cec65c265c7b1
bistmaster <bethoveen.todino@gmail.com>
2020-03-02 20:43:22 +0800

    Edit companies

--- cypress/integration/empower-admin/AdminDashboard/Companies/edit-company.spec.js
+++ cypress/integration/empower-admin/AdminDashboard/Companies/edit-company.spec.js
@@ -1,6 +1,5 @@
 const faker = require("faker");
-const { subEventId, companyInfo, phone } = Cypress.env();
-const companyName = faker.company.companyName(0);
+const { companyNameToEdit, companyNameNew } = Cypress.env("companyInfoEdit");
 describe("Series Admin Dashboard - Companies", () => {
   before(() => {
     // Series Admin User
@@ -11,85 +10,44 @@ describe("Series Admin Dashboard - Companies", () => {
     cy.restoreLocalStorageCache();
   });
 
-  it("Adding Company", () => {
+  it("Edit Company", () => {
     cy.verifyNameAndUrl("Companies", "/companies");
-    cy.contains("a", "+ New Company").click();
-    cy.get("input[name='company.name']").type(companyName);
-    cy.get("select[name='companyRegistration.subEventId']").select(subEventId);
-    cy.get("div.autocomplete-select__control").should("be.visible");
-    cy.get("div.autocomplete-select__control").click();
-    cy.get("div.autocomplete-select__value-container").should("be.visible");
-    cy.get("div.autocomplete-select__input > input").type(
-      companyInfo.teamCaptain
-    );
+    cy.search("div.event-item-container", companyNameToEdit);
 
     cy.server();
-    cy.route(
-      "GET",
-      `/dev/users/searchTcUsers?search=${companyInfo.teamCaptain}&subEventId=${subEventId}`
-    ).as("searchCaptain");
-    cy.wait("@searchCaptain");
-    cy.get("@searchCaptain").then(response => {
-      cy.get("div.autocomplete-select__menu-list").should("be.visible");
-      cy.get("div.autocomplete-select__option").click();
+    cy.route("GET", `${Cypress.config().baseUrl}/dev/*`).as("getCompanyInfo");
+    cy.get("span.action-buttons > a")
+      .first()
+      .click({ force: true });
+    cy.wait("@getCompanyInfo");
+    cy.get("@getCompanyInfo").then(response => {
+      expect(response.status).to.eq(200);
+      cy.get("div.MuiDrawer-paper").should("be.visible");
     });
-
-    cy.phoneSelect("div[id='company.phoneCountryCode']", "Philippines");
-    cy.get("input[name='company.phone']").type(phone);
+    cy.get("input[name='company.name']").clear();
+    cy.get("input[name='company.name']").type(companyNameNew);
     cy.phoneSelect(
       "div[id='companyRegistration.mobileNumberCountryCode']",
-      "China"
+      "Taiwan"
     );
-    cy.get("input[name='companyRegistration.mobileNumber']").type(phone);
+    cy.get("input[name='company.address']").clear();
     cy.get("input[name='company.address']").type(
       faker.address.secondaryAddress()
     );
+    cy.get("input[name='company.city']").clear();
     cy.get("input[name='company.city']").type(faker.address.city());
+    cy.get("input[name='company.zipCode']").clear();
     cy.get("input[name='company.zipCode']").type(faker.address.zipCode());
-    cy.get("select[name='company.country']").select(companyInfo.country);
-    cy.get("select[name='company.state']").select(companyInfo.state);
+    cy.get("input[name='company.hrFirstName']").clear();
     cy.get("input[name='company.hrFirstName']").type(faker.name.firstName());
+    cy.get("input[name='company.hrLastName']").clear();
     cy.get("input[name='company.hrLastName']").type(faker.name.lastName());
-    cy.phoneSelect("div[id='company.hrPhoneNumberCountryCode']", "Hong Kong");
-    cy.get("input[name='company.hrPhoneNumber']").type(phone);
-    cy.get("input[name='company.ceoName']").type(faker.name.findName());
-    cy.get("select[name='company.annualRevenue']").select(
-      companyInfo.annualRevenue
-    );
-    cy.get("select[name='company.industryCategory']").select(
-      companyInfo.industryCategory
-    );
-    cy.get("input[name='companyRegistration.estimatedNumMembers']").type(
-      faker.random.number({ min: 4, max: 6 })
-    );
-    cy.get("input[name='companyRegistration.customPrice']").type(
-      faker.commerce.price()
-    );
-    cy.get("select[name='companyRegistration.whoPays']").select(
-      companyInfo.whoPays
-    );
-    cy.get("select[name='companyRegistration.paymentMethod']").select(
-      companyInfo.paymentMethod
-    );
-    cy.get("textarea[name='companyRegistration.paymentNotes']").type(
-      faker.lorem.sentence()
-    );
-    cy.get("input[name='generatedCustomFields[0].entry[0]']").type(
-      companyInfo.customInformation[0]
-    );
-    cy.get("select[name='generatedCustomFields[1].entry[0]']").select(
-      companyInfo.customInformation[1]
-    );
 
-    cy.get(
-      ".radio_button > .inline_radio > :nth-child(3) > .checkmark"
-    ).click();
-
-    cy.get("button[type=submit]").click();
     cy.server();
-    cy.route("POST", `${Cypress.config().baseUrl}/dev/companyRegistrations`).as(
-      "companyRegistrations"
-    );
+    cy.route(
+      "PUT",
+      `${Cypress.config().baseUrl}/dev/companyRegistrations/*`
+    ).as("companyRegistrations");
     cy.get("button[type=submit]").click();
     cy.wait("@companyRegistrations");
     cy.get("@companyRegistrations").then(response => {
@@ -99,10 +57,8 @@ describe("Series Admin Dashboard - Companies", () => {
   });
 
   it("Search Company", () => {
-    cy.search("div.event-item-container", companyName);
+    cy.search("div.event-item-container", companyNameNew);
   });
-
-  it("Edit Company", () => {});
 
   afterEach(() => {
     cy.saveLocalStorageCache();
